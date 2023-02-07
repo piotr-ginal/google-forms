92271c6b504dacbd076b467367b53090fa9e4fc5
Piotr Ginał <70772418+piotr-ginal@users.noreply.github.com>
2023-02-07 17:11:19 +0100

    Add list_form_questions example

---
+++ examples/list_form_questions.py
@@ -0,0 +1,26 @@
+"""
+This scripts will list all questions from given form and print out their representation
+"""
+
+from google_forms.form_parsing import get_google_form
+
+
+def main():
+    form_id = "form_id"  # eg. 1AFEpGLSd4wrKbTMpGEPwtX3Lvk_vnWv3KuUAzjQgY9UxK6A-PQae0zw
+
+    form = get_google_form(form_id)
+
+    print(form)
+
+    for question in form.questions:
+        print(question)
+
+        if "answers" not in question.__annotations__:
+            continue
+
+        for index, answer in enumerate(question.answers, start=1):
+            print(f"\t- {index} - {answer}")
+
+
+if __name__ == "__main__":
+    main()
